6e7c4fc4e092bfcb52df870aa219aaa672f2aebc
pyfbsdk59 <pyfbsdk59@gmail.com>
2024-04-29 13:55:49 +0800

    4/29 pages 1

--- app.vue
+++ app.vue
@@ -1,10 +1,7 @@
 <script setup>
-import Stock6Sign2403 from '../components/Stock6Sign2403.vue'
+//import Stock6Sign2403 from '../components/Stock6Sign2403.vue'
 </script>
 <template>
   <NuxtPage />
-  <br>
-  <div>
-    <Stock6Sign2403 />
-  </div>
+
 </template>
