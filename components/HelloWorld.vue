3f32f2cb4be122063055154c4e56e4bbac27dc87
pyfbsdk59 <pyfbsdk59@gmail.com>
2024-04-29 12:40:42 +0800

    4/29 4

---
+++ components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfullly come to 總幹事六大指標 選股系統服務！
+      <br />
+      Visit the following websites to get more Info!
+      <a href="https://shopee.tw/abedul59" target="_blank" rel="noopener">Shopee</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Ruten</a>.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
