075e78f50ab743a0463c24ec3ee050ac9fc70649
pyfbsdk59 <pyfbsdk59@gmail.com>
2024-04-30 09:24:21 +0800

    4/30 2

--- app.vue
+++ app.vue
@@ -2,7 +2,18 @@
 
 </script>
 <template>
-  <div>
+    <div class="greetings">
+      <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfullly come to 總幹事六大指標 選股系統服務！
+      <br />
+      Visit the following websites to get more Info!
+      <a href="https://shopee.tw/abedul59" target="_blank" rel="noopener">Shopee</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Ruten</a>.
+    </h3>
+  </div>
+  <br>
+  <div class="center">
   <header>
     <ul>
       <li><NuxtLink to="/">Home</NuxtLink></li>
@@ -20,3 +31,35 @@
   </NuxtLayout>
 </div>
 </template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.center {
+    text-align: center;
+}
+
+
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
